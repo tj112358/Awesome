4105ba9363b9287292ae747546829574500f1af7
Theadora Yocum <theayocum@mac.com>
2025-11-06 11:32:45 -0500

    Update after buttons lesson

--- Awesome/ContentView.swift
+++ Awesome/ContentView.swift
@@ -8,12 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = "Skibidi Sigma"
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+            HStack {
+                Button("Awesome") {
+                    message = "Awesome!"
+                }
+                Button("Great") {
+                    message = "Great!"
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.orange)
         }
         .padding()
     }
