fdb13bf07b33c7500beeb783ad4f4b8f6b73ac59
Theadora Yocum <theayocum@mac.com>
2025-11-06 11:16:03 -0500

    Initial Commit

---
+++ Awesome/AwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwesomeApp.swift
+//  Awesome
+//
+//  Created by Thea Yocum on 11/6/25.
+//
+
+import SwiftUI
+
+@main
+struct AwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
